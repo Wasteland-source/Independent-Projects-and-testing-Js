a23696377a276e85d7f6bb5324f380327f267fde
Wasteland-source <54219063+Wasteland-source@users.noreply.github.com>
2019-09-17 22:14:22 +0100

    Create Number-guessing-game-V1.0.js

---
+++ Number-guessing-game-V1.0.js
@@ -0,0 +1,48 @@
+//A game inwhich you input a number and it tells you if it's higher or lower
+
+const readlineNum = require('readline')
+//Imports readline library
+
+let guesses = 0
+//A variable that will count the guesses used
+
+const randomNumber = Math.floor(Math.random() * 100)
+//Generates a random number
+
+let numberInput = readlineNum.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+//Opens an input and preps for output
+
+numberInput.setPrompt('Please enter a guess from 0-100:');
+//Set's a prompt asking for the users guess
+
+numberInput.prompt();
+//Print's the prompt
+
+numberInput.on('line', function(numberIn) {
+  //Detects when the prompt event is used
+
+  if (guesses === 10) {
+    console.log('Game over, You\'ve ran out of guesses')
+    guesses++
+    process.exit()
+  } else if (numberIn > randomNumber) {
+    console.log('Your guess was too high')
+    guesses++
+    console.log(`You have used ${guesses} guesses`)
+    numberInput.prompt()
+  } else if (numberIn < randomNumber) {
+    console.log('Your guess was too low')
+    guesses++
+    console.log(`You have used ${guesses} guesses`)
+    numberInput.prompt()
+  } else {
+    console.log('Congratulations! You\'ve guessed the number!')
+    guesses++
+    console.log(`You guessed the number in ${guesses} guesses`)
+    process.exit()
+  }
+});
+//An if/elseif/else statementthat checks if the player has guesses remaining, If the number is too high or too low and if none of those are true outputs a victory message
